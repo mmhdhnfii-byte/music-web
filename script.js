ef145286b83c75f54ffc5710684c64a125e0d9d5
mmhdhnfii-byte <mmhdhnfii@gmail.com>
2026-01-28 06:23:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const lyricsDiv = document.getElementById('lyrics');
 
 // Lirik lagu dengan waktu (detik)
 const lyrics = [
-  { time: 6, text: "mengapa tuhan pertemuka" },
+  { time: 0, text: "mengapa tuhan pertemuka" },
   { time: 7, text: "kita yang tak mungkin menyatu" },
-  { time: 6, text: "aku yang telah terikat janji " },
-  { time: 4, text: "engkau pun begitu" },
-  { time: 7, text: ".............."},
+  { time: 15, text: "aku yang telah terikat janji " },
+  { time: 21, text: "engkau pun begitu" },
+  { time: 22, text: ".............."},
 ];
 
 audio.addEventListener("timeupdate", () => {
@@ -33,6 +33,7 @@ audio.addEventListener('timeupdate', () => {
   }
 
 });
+
 
 
 
